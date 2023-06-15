ead3a1637107214322c22e996e755a0c31e0e4e5
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2023-06-15 09:56:43 +0200

    HomePageComponent

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HomePageComponent
+  ]
+})
+export class GifsModule { }
