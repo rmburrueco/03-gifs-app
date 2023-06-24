011e5b112845484c26429708b712b478ff219d63
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2023-06-24 13:38:21 +0200

    ViewChild nos sirve para poder tomar una referencia local HTML

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-box',
@@ -7,11 +7,20 @@ import { Component } from '@angular/core';
         <input type="text"
             class="form-control"
             placeholder="Buscar gifs..."
+            (keyup.enter)="searchTag()"
+            #txtTagInput
         >
     `
 })
 
 export class SearchBoxComponent {
+@ViewChild('txtTagInput') //Nos sirve para poder tomar una refencia local HTML
+    public tagInput!: ElementRef<HTMLInputElement>;  
+    
     constructor() { }
     
+    searchTag(): void{
+        const newTag = this.tagInput.nativeElement.value;
+        console.log({ newTag });
+    }
 }
