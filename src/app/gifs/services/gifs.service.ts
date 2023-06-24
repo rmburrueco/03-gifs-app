1cd8d2a2cc127e174853b567fba33a15e8ab1325
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2023-06-24 15:55:03 +0200

    Petición HTTP promise

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'ApcSkiWk0qDJTzVf7s7KgUrwDJNXHj8S';
 
   constructor() { }
 
@@ -13,10 +14,25 @@ export class GifsService {
     return [...this._tagsHistory]; //hace una copia del array (en lugar de pasar por referencia)
   }
 
-  searchTag( tag:string ):void{
-    this._tagsHistory.unshift( tag );
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
 
-    console.log(this._tagsHistory);
+    if ( this._tagsHistory.includes(tag) ){
+      this._tagsHistory = this.tagsHistory.filter( (oldTag) => oldTag !== tag );
+    }
+
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+  }
+
+  async searchTag( tag:string ): Promise<void> {
+    if(tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=ApcSkiWk0qDJTzVf7s7KgUrwDJNXHj8S&q=valorant&limit=10')
+      .then( resp => resp.json() )
+      .then( data => console.log(data));
+
   }
 
 }
