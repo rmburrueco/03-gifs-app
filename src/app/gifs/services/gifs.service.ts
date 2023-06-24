30d15e3f0e28789eec53cc4aa2858309784c8db2
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2023-06-24 16:25:00 +0200

    Tipado de las interfaces con https://app.quicktype.io/ y con la extensión Paste JSON as Code

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  public gifList: Gif[] = [];
   private _tagsHistory: string[] = [];
   private apiKey:       string = 'ApcSkiWk0qDJTzVf7s7KgUrwDJNXHj8S';
   private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
@@ -36,9 +38,10 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag);
 
-    this.http.get(`${ this.serviceUrl }/search`, { params } ) //sería {params: params} redundante en ECMS6
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params } ) //sería {params: params} redundante en ECMS6
       .subscribe( resp =>{
-        console.log(resp);
+        this.gifList = resp.data;
+        console.log( {gifs: this.gifList} );      
       });
     
     
