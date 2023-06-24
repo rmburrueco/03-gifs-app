5f53b7d25f63925d3633ca9bbe0f2b00819d9efd
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2023-06-24 14:07:58 +0200

    Servicio para el sidebar almacene las búsquedas

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
     selector: 'gifs-search-box',
@@ -14,13 +15,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 
 export class SearchBoxComponent {
-@ViewChild('txtTagInput') //Nos sirve para poder tomar una refencia local HTML
-    public tagInput!: ElementRef<HTMLInputElement>;  
-    
-    constructor() { }
-    
-    searchTag(): void{
+    @ViewChild('txtTagInput') //Nos sirve para poder tomar una refencia local HTML
+    public tagInput!: ElementRef<HTMLInputElement>;
+
+    constructor(private gifsService: GifsService) { }
+
+    searchTag(): void {
         const newTag = this.tagInput.nativeElement.value;
-        console.log({ newTag });
+        this.gifsService.searchTag(newTag);
+        this.tagInput.nativeElement.value = '';
     }
 }
