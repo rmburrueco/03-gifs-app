1a0ca762846f327dc823bc4dd1a693f766f2af2f
rmburrueco <rmburrueco@gmail.com>
2023-06-27 10:04:44 +0200

    Nuevo componente tarjeta de gif

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -3,11 +3,9 @@ import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-card-list',
-  templateUrl: './card-list.component.html'
+  templateUrl: './card-list.component.html',
 })
 export class CardListComponent {
-
   @Input()
   public gifs: Gif[] = [];
-
 }
