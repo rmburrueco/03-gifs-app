b28255c45182ba436059ab4f643e1024b2caf338
rmburrueco <rmburrueco@gmail.com>
2023-06-27 11:33:51 +0200

    Añadir un loader.svg previo a la carga de la img

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,24 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home/home-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
-
-
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
+    CardListComponent,
+    GifsCardComponent,
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent,
-    GifsCardComponent
   ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    HomePageComponent
-  ]
+  imports: [CommonModule, SharedModule],
+  exports: [HomePageComponent],
 })
-export class GifsModule { }
+export class GifsModule {}
